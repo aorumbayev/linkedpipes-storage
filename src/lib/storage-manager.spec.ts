34e28ad6f4a688bf7653882f19b55a2a93fb0a86
Altynbek Orumbayev <aorumbaev95@gmail.com>
2019-10-27 16:08:31 +0300

    :white_check_mark: Additional unit tests

--- src/lib/storage-manager.spec.ts
+++ src/lib/storage-manager.spec.ts
@@ -2,6 +2,7 @@ import test from 'ava';
 import auth from 'solid-auth-cli';
 import uuid from 'uuid';
 import {
+  AccessControlNamespace,
   ResourceConfig,
   SolidResourceType,
   StorageFileManager
@@ -30,6 +31,26 @@ const folderConfigurationResource: ResourceConfig = {
   webID: SOLID_WEBID
 };
 
+async function createResource(t: any, input: any, expected: any): Promise<any> {
+  const result = await StorageFileManager.createResource(input);
+  t.is(result.status, expected);
+}
+
+async function deleteResource(t: any, input: any, expected: any): Promise<any> {
+  const result = await StorageFileManager.deleteResource(input);
+  t.is(result.status, expected);
+}
+
+async function resourceExists(t: any, input: any, expected: any): Promise<any> {
+  const result = await StorageFileManager.resourceExists(input);
+  t.is(result.status, expected);
+}
+
+async function updateACL(t: any, input: any, expected: any): Promise<any> {
+  const result = await StorageFileManager.updateACL(input);
+  t.is(result.status, expected);
+}
+
 test.before(async () => {
   session = await auth.currentSession();
   if (!session) {
@@ -41,30 +62,86 @@ test.before(async () => {
   }
 });
 
-test.serial('createFolderResource', async t => {
-  const result = await StorageFileManager.createResource(
-    folderConfigurationResource
-  );
-  t.is(result.status, 201);
-});
+test.serial(
+  'createFolderResource',
+  createResource,
+  folderConfigurationResource,
+  201
+);
 
-test.serial('deleteFolderResource', async t => {
-  const result = await StorageFileManager.deleteResource(
-    folderConfigurationResource
-  );
-  t.is(result.status, 200);
-});
+test.serial(
+  'folderResourceExists',
+  resourceExists,
+  folderConfigurationResource.resource.path,
+  200
+);
 
-test.serial('createFileResource', async t => {
-  const result = await StorageFileManager.createResource(
-    fileConfigurationResource
-  );
-  t.is(result.status, 201);
-});
+test.serial(
+  'folderResourceUpdateACL',
+  updateACL,
+  {
+    webID: SOLID_WEBID,
+    controlModes: [AccessControlNamespace.Read, AccessControlNamespace.Write],
+    resource: {
+      ...folderConfigurationResource.resource,
+      isPublic: true
+    }
+  },
+  201
+);
 
-test.serial('deleteFileResource', async t => {
-  const result = await StorageFileManager.deleteResource(
-    fileConfigurationResource
-  );
-  t.is(result.status, 200);
-});
+test.serial(
+  'deleteFolderResource',
+  deleteResource,
+  folderConfigurationResource,
+  200
+);
+
+test.serial(
+  'folderResourceDoesNotExist',
+  resourceExists,
+  fileConfigurationResource.resource.path,
+  404
+);
+
+test.serial(
+  'createFileResource',
+  createResource,
+  fileConfigurationResource,
+  201
+);
+
+test.serial(
+  'fileResourceExists',
+  resourceExists,
+  fileConfigurationResource.resource.path,
+  200
+);
+
+test.serial(
+  'fileResourceUpdateACL',
+  updateACL,
+  {
+    webID: SOLID_WEBID,
+    controlModes: [AccessControlNamespace.Read, AccessControlNamespace.Write],
+    resource: {
+      ...fileConfigurationResource.resource,
+      isPublic: true
+    }
+  },
+  201
+);
+
+test.serial(
+  'deleteFileResource',
+  deleteResource,
+  fileConfigurationResource,
+  200
+);
+
+test.serial(
+  'fileResourceDoesNotExist',
+  resourceExists,
+  fileConfigurationResource.resource.path,
+  404
+);
